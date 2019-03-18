aaecd957f58cf6def3fc34a909995ede394a0e60
PapuutekAPT <dirbachd123@gmail.com>
2019-03-18 23:57:42 +0100

    ALPHA 0.02
    - zmieniono układ strony
    - dodano automatyczne numerowanie grupy nadanej
    - dodano warunek czy pola nie są puste
    - dodano historię zalogowanych łączności
    - dodano wywołanie przycisku przy pomocy klawisza ENTER
    - zmieniono tło strony
    - dodano czyszenie pól po zapisie

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+logs = [];
+
 document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("sl_btn").addEventListener("click", save_log);
 }, false);
 
+function handleEnter(e){
+	if (e.keyCode === 13) {
+		document.getElementById("sl_btn").click();
+	}
+}
+
 function GetText(id) {
 	return document.getElementById(id).value;
 }
-
+function clear_field(){
+	document.getElementById("izk").value ="";
+	document.getElementById("irn").value ="59";
+	document.getElementById("iro").value ="59";
+	document.getElementById("igo").value ="";
+}
 
 function save_log() {
-	var log = {
+	 log = {				// w tym miejscu zostały użyte skróty. przykładowo: zk ozn. Znak Korespondenta
+							//									 a izk - skrót od input znak korespondenta.
+		zn: GetText("izn"),
 		zk: GetText("izk"),
 		rn: GetText("irn"),
 		gn: GetText("ign"),
@@ -17,11 +32,59 @@ function save_log() {
 		po: GetText("ipo"),
 		ea: GetText("iea")
 	};
-	console.log("Znak korespondenta: " + log.zk);
-	console.log("RTSn: " + log.rn);
-	console.log("Grupa nadawcza: " + log.gn);
-	console.log("RTSo: " + log.ro);
-	console.log("Grupa odebrana: " + log.go);
-	console.log("Pasmo: " + log.po);
-	console.log("Emisja: " + log.ea);
+	var isOK = true;
+	if(log.zn == "" || log.zk == "" || log.rn == "" || log.ro == "" || log.go == "")
+		isOK = false;
+
+	if(!isOK)
+		alert("Wypełnij wszystkie pola!");
+	else{
+
+		logs[logs.length] = log;
+
+		var new_gn= parseInt(log.gn)+1;
+		var str_new_gn ="";
+		for(var i = 0; i <3 - (new_gn+"").length;i++)
+			str_new_gn += "0";
+		str_new_gn += new_gn;
+		document.getElementById("ign").value = str_new_gn;
+		clear_field();
+
+
+		console.log("Znak korespondenta: " + log.zk);
+		console.log("Grupa odebrana: " + log.go);
+		console.log("RTSo: " + log.ro);
+		console.log("Grupa nadawcza: " + log.gn);
+		console.log("RTSn: " + log.rn);
+		console.log("Pasmo: " + log.po);
+		console.log("Emisja: " + log.ea);
+		console.log("Znak Nadawcy: " + log.zn);
+
+		view_log();
+	}
+}
+wiersz = "";
+function view_log() {
+
+	var table = document.getElementById("log_table");
+	var row = table.insertRow(table.length);
+
+	var zk = row.insertCell(0);
+	var go = row.insertCell(1);
+	var ro = row.insertCell(2);
+	var gn = row.insertCell(3);
+	var rn = row.insertCell(4);
+	var po = row.insertCell(5);
+	var ea = row.insertCell(6);
+	var zn = row.insertCell(7);
+
+	zk.innerHTML = log.zk;
+	go.innerHTML = log.go;
+	ro.innerHTML = log.ro;
+	gn.innerHTML = log.gn;
+	rn.innerHTML = log.rn;// git tylko strongi to uppercase trzeba dac //DODANE 
+	po.innerHTML = log.po;
+	ea.innerHTML = log.ea; //ESSA JA BOI
+	zn.innerHTML = log.zn;
+
 }

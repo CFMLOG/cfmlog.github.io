4617d5444fd36bbb18ff264646c15b7dbb8fc746
PapuutekAPT <dirbachd123@gmail.com>
2019-03-19 22:32:20 +0100

    ALPHA 0.03
    - poprawiono wygląd strony
    - zmieniono nazwę na CFMLOG
    - dodano możliwość zapisu do pliku ADIF
    - poprawiono responsywność strony
    - dodano wygodną kolejność "tabulatorów"

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 logs = [];
-
+//									PRZYCISKI
 document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("sl_btn").addEventListener("click", save_log);
 }, false);
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("sf_btn").addEventListener("click", save_to_file_open);
+}, false);
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("return_btn").addEventListener("click", save_to_file_return);
+}, false);
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("save_btn").addEventListener("click", save_to_file);
+}, false);
 
-function handleEnter(e){
+//			PRZYCISK DODAJ QSO POD ENTEREM
+document.addEventListener("keypress", function(e){
 	if (e.keyCode === 13) {
 		document.getElementById("sl_btn").click();
 	}
-}
+});
 
 function GetText(id) {
 	return document.getElementById(id).value;
 }
+// CZYSZCZENIE PÓL PO DODANIU QSO
 function clear_field(){
 	document.getElementById("izk").value ="";
 	document.getElementById("irn").value ="59";
@@ -21,6 +32,7 @@ function clear_field(){
 }
 
 function save_log() {
+	var date = new Date();
 	 log = {				// w tym miejscu zostały użyte skróty. przykładowo: zk ozn. Znak Korespondenta
 							//									 a izk - skrót od input znak korespondenta.
 		zn: GetText("izn"),
@@ -30,8 +42,10 @@ function save_log() {
 		ro: GetText("iro"),
 		go: GetText("igo"),
 		po: GetText("ipo"),
-		ea: GetText("iea")
-	};
+		ea: GetText("iea"),
+		cs: (date.getHours().toString()+date.getMinutes().toString()+date.getSeconds().toString()),
+		da: (date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate())
+		};
 	var isOK = true;
 	if(log.zn == "" || log.zk == "" || log.rn == "" || log.ro == "" || log.go == "")
 		isOK = false;
@@ -49,18 +63,8 @@ function save_log() {
 		str_new_gn += new_gn;
 		document.getElementById("ign").value = str_new_gn;
 		clear_field();
-
-
-		console.log("Znak korespondenta: " + log.zk);
-		console.log("Grupa odebrana: " + log.go);
-		console.log("RTSo: " + log.ro);
-		console.log("Grupa nadawcza: " + log.gn);
-		console.log("RTSn: " + log.rn);
-		console.log("Pasmo: " + log.po);
-		console.log("Emisja: " + log.ea);
-		console.log("Znak Nadawcy: " + log.zn);
-
 		view_log();
+		document.getElementById("izk").focus();
 	}
 }
 wiersz = "";
@@ -74,17 +78,64 @@ function view_log() {
 	var ro = row.insertCell(2);
 	var gn = row.insertCell(3);
 	var rn = row.insertCell(4);
-	var po = row.insertCell(5);
-	var ea = row.insertCell(6);
-	var zn = row.insertCell(7);
+	var da = row.insertCell(5);
+	var cs = row.insertCell(6);
+	var po = row.insertCell(7);
+	var ea = row.insertCell(8);
+	var zn = row.insertCell(9);
 
 	zk.innerHTML = log.zk;
 	go.innerHTML = log.go;
 	ro.innerHTML = log.ro;
 	gn.innerHTML = log.gn;
-	rn.innerHTML = log.rn;// git tylko strongi to uppercase trzeba dac //DODANE 
+	rn.innerHTML = log.rn;
+	da.innerHTML = log.da;
+	cs.innerHTML = log.cs;
 	po.innerHTML = log.po;
-	ea.innerHTML = log.ea; //ESSA JA BOI
+	ea.innerHTML = log.ea;
 	zn.innerHTML = log.zn;
+
+}
+//	POJAWIENIE SIĘ PANELU ZAPISU LOGÓW PO PRZYCISKU ZAPISZ LOG
+function save_to_file_open(){
+	if(logs.length <1) alert("NIE ZALOGOWAŁEŚ JESZCZE ŻADNEGO QSO!"); else{
+		//document.getElementById("blurpage").style.display = "block";
+		document.getElementById("savetofile_container").style.display = "block";
+	}
+}
+//	ZAMKNIĘCIE PANELU ZAPISU LOGÓW PO PRZYCISKU POWROT
+function save_to_file_return(){
+	//document.getElementById("blurpage").style.display = "none";
+	document.getElementById("savetofile_container").style.display = "none";
+}
+//	AKCJA PRZYCISKU ZAPISZ
+function save_to_file(){
+	var file_format = document.getElementById("file_format");
+	var operator = document.getElementById("operator").value;
+	var comment = document.getElementById("comment").value;
+	file_format = file_format.options[file_format.selectedIndex].value;
+	switch(file_format){
+		case "adif":
+			adif_save(operator,comment);
+			break;
+	}
+
+}
+// ZAPIS PLIKU ADIF
+function adif_save(operator, comment){
+	var date = new Date;
+	console.log("aaa");
+	var text = "ADIF 2.0\nWYGENEROWANY PRZEZ CFMLOG http://github.com/kpxdi/glolog \n<EOH>\n";
+	for(var i =0;i<logs.length;i++){
+		text += "<QSO_DATE:8>"+logs[i].date + " <TIME_ON:6>"+ logs[i].time +" <BAND:3>"+ logs[i].po +" <FREQ:5>3.700 <MODE:3>"+ logs[i].ea;
+		text +=	"<CALL:6>" + logs[i].zk + " <RST_RCVD:2>" + logs[i].ro + " <SRX:3>" + logs[i].go + " <RST_SENT:2>"+ logs[i].rn +" <STX:9>"+ logs[i].gn + logs[i].zn +"<COMMENT:5>"+ comment;
+		text += "<OPERATOR:5>"+ operator +"<CONTEST_ID:5> 2019 <EOR>\n";
+	}
+    blob = new Blob([text], { type: 'text/plain' }),
+    link = document.createElement('a');
+	link.download = date.getFullYear()+":"+date.getMonth()+":"+date.getDate()+":"+date.getHours()+":"+date.getMinutes()+".adi";
+	link.href = (window.webkitURL || window.URL).createObjectURL(blob);
+	link.dataset.downloadurl = ['text/plain', link.download, link.href].join(':');
+	link.click();
 
 }
